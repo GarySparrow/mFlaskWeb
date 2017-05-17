f49de40ef5bb1145f39a3f7ac9465f1b622f7250
Gary <690131179@qq.com>
2017-05-17 19:29:04 +0800

    update

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Admin'
+    FLASKY_MAIL_SENDER = 'Blog Admin <690131179@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
