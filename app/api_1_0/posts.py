8e82d2482e35fcc3be4cc354b5724f5772821e19
Gary <690131179@qq.com>
2017-05-17 20:22:01 +0800

    update

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -58,8 +58,8 @@ def new_post():
     img = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9']
 
     idx = 0
-    imgs_root = 'static/'
-    video_root = 'static/'
+    imgs_root = '/static/'
+    video_root = '/static/'
     imgs_path = []
     video_path = None
     for s in img:
